8b346b10df56a8ac711ebd5dc62e69e5ce4e4d7f
Isaac <80630060+isaackohen@users.noreply.github.com>
2022-01-30 00:47:27 +0100

    Update Score.svelte

--- src/components/control/Score.svelte
+++ src/components/control/Score.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex items-center justify-center h-full bg-gray-600 text-white">
   <div class="flex flex-col items-center">
-    <div class="text-xs font-nunito_light text-center>Holly Dollars</div>
+    <div class="text-xs font-nunito_light text-center">Holly Dollars</div>
     <div class="font-roboto_bold text-4xl w-full text-center">
       {$game.score}
     </div>
